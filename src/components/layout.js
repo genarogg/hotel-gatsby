1dd06a516815220248b4aa0101958030932c21e1
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-06 12:42:24 -0400

    Agregando CSS Global

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,44 @@ import React from "react"
 import Header from "../components/header"
 import Helmet from "react-helmet"
 
+import { Global, css } from "@emotion/core"
+
 const Layout = props => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+
+          body {
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+          h3 {
+            font-family: "Lato", "PT Sans", sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
       <Helmet>
         <title>Gatsby Hotel</title>
         <link rel="stylesheet" href="../normalize.min.css" />
