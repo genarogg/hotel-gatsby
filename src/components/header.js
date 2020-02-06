efdfe76bb5d68c8d64282fb41dcca624b51f8345
Genaro Gonzalez <genarrogg@gmail.com>
2020-02-06 12:41:51 -0400

    Creando un Layout Principa

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <h1>Desde header</h1>
+  </header>
+)
+
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import Header from "../components/header"
+
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Gatsby Hotel</h1>
+  </Layout>
+)
+
+export default IndexPage
