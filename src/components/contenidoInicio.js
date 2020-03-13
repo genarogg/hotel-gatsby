35819cf99712b5f6ffabd88c4e3e07178969c62b
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-13 07:42:51 -0400

    TextoInicio

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -3,6 +3,22 @@ import { graphql, useStaticQuery } from "gatsby"
 import Image from "gatsby-image"
 import styled from "@emotion/styled"
 
+const TextoInicio = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+
+  p {
+    line-height: 2;
+  }
+`
 
 const ContenidoInicio = () => {
   const informacion = useStaticQuery(graphql`
@@ -24,7 +40,15 @@ const ContenidoInicio = () => {
   const { titulo, contenido, imgen } = informacion.allDatoCmsPagina.nodes[0]
   return (
     <>
-      <h2>titulo</h2>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {titulo}
+      </h2>
       <div>
         <p>{contenido}</p>
         <Image fluid={imagen.fluid} />
