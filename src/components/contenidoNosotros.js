f790ba5e13651a6ef603f9d7dda989ee3a95cfc6
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-13 07:50:32 -0400

    Contenido

--- src/components/contenidoNosotros.js
+++ src/components/contenidoNosotros.js
@@ -4,7 +4,22 @@ import Image from 'gatsby-image';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 
+const Contenido = styled.main`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
 
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 3rem;
+    }
+    p {
+        line-height: 2;
+        margin-top: 2rem;
+    }
+`;
 
 const ContenidoNosotros = () => {
 
