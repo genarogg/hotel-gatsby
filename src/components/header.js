3fe25df019045b844c05d2630513e776bdcda4d3
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-13 07:59:45 -0400

    Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,45 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import { css } from "@emotion/core"
-import Navegacion from "./nav"
+import React from 'react';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+import Navegacion from './nav';
+
+
+const EnlaceHome = styled(Link)`
+    color: #FFF;
+    text-align: center;
+    text-decoration: none;
+`;
 
 const Header = () => {
-  return (
-    <header
-      css={css`
-        background-color: rgba(44, 62, 80);
-        padding: 1rem;
-      `}
-    >
-      <div
-        css={css`
-          max-width: 1200px;
-          margin: 0 auto;
+    return ( 
+        <header
+            css={css`
+                background-color: rgba(44,62,80);
+                padding: 1rem;
+            `}
+        >
+            <div
+                css={css`
+                    max-width: 1200px;
+                    margin: 0 auto;
 
-          @media (min-width: 768px) {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-          }
-        `}
-      >
-        <h1>Hotel Gatsby</h1>
-        <Navegacion />
-      </div>
-    </header>
-  )
+                    @media (min-width: 768px) {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                `}
+            >
+                <EnlaceHome
+                    to='/'
+                ><h1>Hotel Gatsby</h1> </EnlaceHome>
+                
+
+                <Navegacion />
+            </div>
+        </header>
+     );
 }
-
-export default Header
+ 
+export default Header;
