d63b331606671531e53e623f2e00611b755e9460
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-14 12:33:41 -0400

    ListadoHabitaciones

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,17 @@ import ContenidoInicio from '../components/ContenidoInicio';
 import HabitacionPreview from '../components/habitacionPreview';
 import useHabitaciones from '../hooks/use-habitaciones';
 
+const ListadoHabitaciones = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
 
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem
+  }
+`
 
 const IndexPage = () => {
 
