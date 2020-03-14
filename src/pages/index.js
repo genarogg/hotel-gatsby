5d5e1e3bc52181f33358966bb21bdccb7ca87006
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-14 12:33:19 -0400

    HabitacionPreview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,42 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import Layout from '../components/layout';
+import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicio from '../components/ContenidoInicio';
+import HabitacionPreview from '../components/habitacionPreview';
+import useHabitaciones from '../hooks/use-habitaciones';
 
-import ImagenHotel from "../components/imagenHotel"
-import ContenidoInicio from "../components/contenidoInicio"
-const IndexPage = () => (
-  <Layout>
-    <ImagenHotel/>
-    <ContenidoInicio />
-  </Layout>
-)
+
+
+const IndexPage = () => {
+
+  const habitaciones = useHabitaciones();
+
+  console.log(habitaciones);
+  
+
+  return (
+    <Layout>
+        <ImagenHotel />
+  
+        <ContenidoInicio />
+  
+        <h2
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+            font-size: 3rem;
+          `}
+        >Nuestras Habitaciones
+        </h2>
+
+        
+  
+        
+    </Layout> 
+    
+  )
+}
 
 export default IndexPage
