d983adf5bd69891a27dc502632b3d20ad874706c
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-14 12:35:08 -0400

    HabitacionPreview

---
+++ src/components/HabitacionPreview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+
+
+const HabitacionPreview = ({habitacion}) => {
+
+    const { contenido, imagen, titulo, slug } = habitacion;
+
+    return ( 
+        
+        </div>
+     );
+}
+ 
+export default HabitacionPreview;
