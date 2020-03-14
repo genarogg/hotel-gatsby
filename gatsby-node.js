c7ed21729bd59d0c027466e07c74c740d138d892
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-14 12:51:19 -0400

    resultado

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const resultado = await graphql(`
+        query {
+            allDatoCmsHabitacion {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    // console.log(resultado.data.allDatoCmsHabitacion.nodes);
+
+
+}
