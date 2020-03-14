553c5515ff70e19e9aeb60fc346b06738026675e
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-14 12:35:51 -0400

    style component Boton

--- src/components/HabitacionPreview.js
+++ src/components/HabitacionPreview.js
@@ -4,6 +4,18 @@ import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 
+const Boton = styled(Link)`
+    margin-top: 2rem;
+    padding: 1rem;
+    background-color: rgba(44,62,80,.85);
+    width: 100%;
+    color: #FFF;
+    display: block;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    text-align: center;
+`;
 
 const HabitacionPreview = ({habitacion}) => {
 
