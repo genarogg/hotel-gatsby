7db4480d8610d58387656643bf5a878bbe9048d7
Genaro Gonzalez <genarrogg@gmail.com>
2020-03-15 13:12:34 -0400

    footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,64 +1,56 @@
-import React from "react"
-import Header from "../components/header"
-import Helmet from "react-helmet"
-import "../normalize.min.css"
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/core';
+import Header from './header';
+import Footer from './footer';
+import useSeo from '../hooks/use-seo';
 
-import { Global, css } from "@emotion/core"
+const Layout = (props) => {
 
-const Layout = props => {
-  return (
-    <>
-      <Global
-        styles={css`
-          html {
-            font-size: 62.5%;
-            box-sizing: border-box;
-          }
-          *,
-          *:before,
-          *:after {
-            box-sizing: inherit;
-          }
-          body {
-            font-size: 18px;
-            font-size: 1.8rem;
-            line-height: 1.5;
-            font-family: "PT Sans", sans-serif;
-          }
-          h1,
-          h2,
-          h3 {
-            margin: 0;
-            line-height: 1.5;
-          }
+   
+    return ( 
+        <>
+            <Global
+                styles={css`
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
+                    body {
+                        font-size: 18px;
+                        font-size: 1.8rem;
+                        line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding:0;
+                    }
+                `}
+            />
+         
 
-          h1,
-          h2 {
-            font-family: "Lato", "Roboto", serif;
-          }
-          h3 {
-            font-family: "Lato", "PT Sans", sans-serif;
-          }
-
-          ul {
-            list-style: none;
-            margin: 0;
-            padding: 0;
-          }
-        `}
-      />
-      <Helmet>
-        <title>Gatsby Hotel</title>
-
-        <link
-          href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <Header />
-      {props.children}
-    </>
-  )
+            <Header />
+                {props.children}
+            <Footer 
+                title={title}
+            />
+        </>
+     );
 }
-
-export default Layout
+ 
+export default Layout;
